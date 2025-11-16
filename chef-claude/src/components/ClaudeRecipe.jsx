ba3e40035eee25929f1d655f18e7dcfd78de3472
nick990 <nicolo.festa90@gmail.com>
2025-11-17 00:05:22 +0100

    chef-claude: format markdown using react-markdown

--- chef-claude/src/components/ClaudeRecipe.jsx
+++ chef-claude/src/components/ClaudeRecipe.jsx
@@ -1,3 +1,9 @@
+import Markdown from "react-markdown";
+
 export default function ClaudeRecipe(props) {
-  return <section>{props.recipe}</section>;
+  return (
+    <section className="suggested-recipe-container">
+      <Markdown>{props.recipe}</Markdown>
+    </section>
+  );
 }
