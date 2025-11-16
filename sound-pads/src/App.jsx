352867df4ef8c2cd4cc5eeb1501fd384bcbd228d
nick990 <nicolo.festa90@gmail.com>
2025-11-16 16:38:52 +0100

    challenge 1

--- sound-pads/src/App.jsx
+++ sound-pads/src/App.jsx
@@ -1,9 +1,14 @@
-import pads from "./assets/pads";
-
+import padsData from "./assets/pads";
+import { useState } from "react";
 export default function App() {
+  const [pads, setPads] = useState(padsData);
+
+  const padsButtons = pads.map((pad) => {
+    return <button key={pad.id}></button>;
+  });
   return (
     <main>
-      <div className="pad-container">{/* <button>s go here */}</div>
+      <div className="pad-container">{padsButtons}</div>
     </main>
   );
 }
