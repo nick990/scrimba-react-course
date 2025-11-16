4034e7afa81fd4aa14e7b9fb841a9b2fa87075e6
nick990 <nicolo.festa90@gmail.com>
2025-11-16 16:47:10 +0100

    challenge 2

--- sound-pads/README.md
+++ sound-pads/README.md
@@ -5,3 +5,10 @@
 1. Initialize state with the default value of the array pulled in from pads.js
 2. Map over that state array and display each one as a `<button>` (CSS is already written for you)\
    (Don't worry about using the "on" or "color" properties yet)
+
+## Challenge 2
+
+Use a ternary to determine the backgroundColor of the buttons:
+
+- If darkMode is true, set them to "#222222"
+- If darkMode is false, set them to "#cccccc"
--- sound-pads/src/App.jsx
+++ sound-pads/src/App.jsx
@@ -1,10 +1,15 @@
 import padsData from "./assets/pads";
 import { useState } from "react";
-export default function App() {
+export default function App({ darkMode }) {
   const [pads, setPads] = useState(padsData);
 
   const padsButtons = pads.map((pad) => {
-    return <button key={pad.id}></button>;
+    return (
+      <button
+        key={pad.id}
+        style={{ backgroundColor: darkMode ? "#222222" : "#cccccc" }}
+      ></button>
+    );
   });
   return (
     <main>
