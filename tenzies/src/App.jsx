f41babf0f370b17589235caf4bfcc290122efe5c
nick990 <nicolo.festa90@gmail.com>
2025-11-24 00:14:13 +0100

    tenzies: challenge 13 finish

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,5 +1,6 @@
 import { nanoid } from "nanoid";
 import { useState } from "react";
+import Confetti from "react-confetti";
 import Die from "./components/Die";
 
 export default function App() {
@@ -39,6 +40,7 @@ export default function App() {
 
   return (
     <main>
+      {gameWon && <Confetti />}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">
         Roll until all dice are the same. Click each die to freeze it at its
