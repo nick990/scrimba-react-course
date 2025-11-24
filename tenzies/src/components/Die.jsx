cd9e2688ae745a7571486d938f3f2c64c764626c
nick990 <nicolo.festa90@gmail.com>
2025-11-24 23:34:07 +0100

    tenzies: accessibility improvements

--- tenzies/src/components/Die.jsx
+++ tenzies/src/components/Die.jsx
@@ -1,6 +1,12 @@
 export default function Die(props) {
   return (
-    <button className={props.isHeld ? "held" : ""} onClick={props.hold}>
+    <button
+      className={props.isHeld ? "held" : ""}
+      onClick={props.hold}
+      aria-pressed={props.isHeld}
+      aria-label={`Die with value ${props.value}, 
+    ${props.isHeld ? "held" : "not held"}`}
+    >
       {props.value}
     </button>
   );
